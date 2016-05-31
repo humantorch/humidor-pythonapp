7a2e080a8549f4b25392073c6538014b2105ad90
scott kosman <kosman.scott@gmail.com>
2016-05-31 22:27:24 +0200

    changed random value generation to include one decimal place

--- humidor.py
+++ humidor.py
@@ -1,39 +1,39 @@
 #!/usr/bin/python
 import MySQLdb
 import time
-from random import randint
-import config #config file containing db connection info
+import random
+import decimal
+import config
 
-# create a separate 'config.py' file and put the following into it (with your own values, obv)
-# pro tip: put config.py and *.pyc in your .gitignore file
-
-
-    # user = "username"
-    # db = "database name"
-    # pword = "database password"
-    # host = "localhost"
-
+#print config.pword
 
 db = MySQLdb.connect(host=config.host,
                      user=config.user,
                      passwd=config.pword,
                      db=config.db)
 
+# you must create a Cursor object. It will let
+#  you execute all the queries you need
 cur = db.cursor()
 nowtime = time.strftime('%Y-%m-%d %H:%M:%S')
 
 #temporary random number generation until device setup
-humid = randint(60,75)
-temp = randint(60,75)
+humid = round(random.uniform(60,75), 1)
+temp = round(random.uniform(60,75), 1)
 
+# Use all the SQL you like
 try:
 	cur.execute("INSERT INTO data(curdate, humidity, temperature) VALUES (%s, %s, %s)",(nowtime, humid, temp))
 	db.commit()
-
+	# cur.execute("SELECT * FROM data")
 except MySQLdb.Error, e:
     try:
         print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
     except IndexError:
         print "MySQL Error: %s" % str(e)
+
+# print all the first cell of all the rows
+# for row in cur.fetchall():
+#     print row[0]
 
 db.close()
