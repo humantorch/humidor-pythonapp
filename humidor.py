6c30267f9d62d546e80ba44eff0b193718de7def
scott kosman <kosman.scott@gmail.com>
2016-05-31 22:28:16 +0200

    clean up python comments

--- humidor.py
+++ humidor.py
@@ -5,15 +5,11 @@ import random
 import decimal
 import config
 
-#print config.pword
-
 db = MySQLdb.connect(host=config.host,
                      user=config.user,
                      passwd=config.pword,
                      db=config.db)
 
-# you must create a Cursor object. It will let
-#  you execute all the queries you need
 cur = db.cursor()
 nowtime = time.strftime('%Y-%m-%d %H:%M:%S')
 
@@ -21,19 +17,13 @@ nowtime = time.strftime('%Y-%m-%d %H:%M:%S')
 humid = round(random.uniform(60,75), 1)
 temp = round(random.uniform(60,75), 1)
 
-# Use all the SQL you like
 try:
 	cur.execute("INSERT INTO data(curdate, humidity, temperature) VALUES (%s, %s, %s)",(nowtime, humid, temp))
 	db.commit()
-	# cur.execute("SELECT * FROM data")
 except MySQLdb.Error, e:
     try:
         print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
     except IndexError:
         print "MySQL Error: %s" % str(e)
-
-# print all the first cell of all the rows
-# for row in cur.fetchall():
-#     print row[0]
 
 db.close()
