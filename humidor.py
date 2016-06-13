5b1db942b609cb8d4d34d7bccb6f73df7d9a3d56
scott kosman <kosman.scott@gmail.com>
2016-06-13 22:32:41 +0200

    finally using actual sensor data now

--- humidor.py
+++ humidor.py
@@ -4,7 +4,40 @@ import time
 import random
 import decimal
 import config
+import sys
+import Adafruit_DHT
 
+# Parse command line parameters for sensor reading
+# TODO: once the final setup is complete hard-code these values
+sensor_args = { '11': Adafruit_DHT.DHT11,
+                '22': Adafruit_DHT.DHT22, # this is the one I'm using
+                '2302': Adafruit_DHT.AM2302 }
+if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
+    sensor = sensor_args[sys.argv[1]]
+    pin = sys.argv[2]
+else:
+    print('usage: sudo ./humidor.py [11|22|2302] GPIOpin#')
+    print('example: sudo ./humidor.py 2302 4 - Read from an AM2302 connected to GPIO #4')
+    sys.exit(1)
+
+
+
+# Try to grab a sensor reading.  Use the read_retry method which will retry up
+# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+# Un-comment the line below to convert the temperature to Fahrenheit.
+temperature = temperature * 9/5.0 + 32
+
+if humidity is not None and temperature is not None:
+    #print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+    humid = humidity
+    temp = temperature
+else:
+    print('Failed to get reading. Try again!')
+    sys.exit(1)
+
+# set up db connection
 db = MySQLdb.connect(host=config.host,
                      user=config.user,
                      passwd=config.pword,
@@ -13,13 +46,11 @@ db = MySQLdb.connect(host=config.host,
 cur = db.cursor()
 nowtime = time.strftime('%Y-%m-%d %H:%M:%S')
 
-#temporary random number generation until device setup
-humid = round(random.uniform(60,75), 1)
-temp = round(random.uniform(60,75), 1)
-
 try:
 	cur.execute("INSERT INTO data(curdate, humidity, temperature) VALUES (%s, %s, %s)",(nowtime, humid, temp))
 	db.commit()
+	#print humid
+	#print temp
 except MySQLdb.Error, e:
     try:
         print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
